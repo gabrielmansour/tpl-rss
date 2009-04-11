7b7d98576e4eb4be0b7b616c91a4395886a206ea
Gabriel Mansour <gabrielmansour@gmail.com>
2009-04-10 21:48:28 -0400

    added ability to pass in library card number & pin as command-line arguments; raises an error if login credentials are incorrect; fixed up generated XML to better adhere to Atom standard; improved shebang

--- tpl-rss.rb
+++ tpl-rss.rb
@@ -1,4 +1,4 @@
-#!/usr/local/bin/ruby
+#!/usr/bin/env ruby
 #
 # == TPL-RSS ==
 #  <http://github.com/gabrielmansour/tpl-rss>
@@ -9,7 +9,9 @@
 require 'rubygems'
 require 'mechanize' # using nokogiri
 
-LIBRARY_CARD, PIN = '0000000000000', '0000' # your personal account credentials
+@LIBRARY_CARD, @PIN = '000000000000000', '0000' # your personal account credentials go here
+
+@LIBRARY_CARD, @PIN = *ARGV unless ARGV.empty? # ..or optionally, you can pass in your credentials when executing this script from the command line
 
 LOGIN_URL = 'https://ezproxy.torontopubliclibrary.ca/login'
 ACCOUNT_URL = 'http://ezproxy.torontopubliclibrary.ca/sso/myacct'
@@ -18,18 +20,20 @@ CATALOGUE_URL = 'http://catalogue.torontopubliclibrary.ca'
 ua = WWW::Mechanize.new
 ua.read_timeout = 10
 
-response = ua.post(LOGIN_URL, :user => LIBRARY_CARD, :pass => PIN, :url => ACCOUNT_URL)
+response = ua.post(LOGIN_URL, :user => @LIBRARY_CARD, :pass => @PIN, :url => ACCOUNT_URL)
+raise "Login Error: Incorrect library card number or PIN." if response.body.match(/Incorrect library card number or PIN. Please try again./)
+
 
 feed = Nokogiri::XML::Builder.new do
   feed( :xmlns => "http://www.w3.org/2005/Atom", :"xml:lang" => "en" ) do |f|
     f.title     "Library books"
     f.subtitle  "From the Toronto Public Library"
     f.updated   Time.now.iso8601
-    f.link      CATALOGUE_URL
     f.link      :rel => 'self', :href => ''
-    f.generator(:href => 'http://github.com/gabrielmansour/tpl-rss'){ text "TPL-RSS" }
-    f.id_       ""
-    f.author    ""
+    f.link      :rel => 'alternate', :type => 'text/html', :href => CATALOGUE_URL
+    f.generator(:uri => 'http://github.com/gabrielmansour/tpl-rss'){ text "TPL-RSS" }
+    f.id_       "tag:torontopubliclibrary.ca,2008:tpl-rss"
+    f.icon      ""
 
     ua.get(CATALOGUE_URL) do |page|
 
@@ -38,20 +42,23 @@ feed = Nokogiri::XML::Builder.new do
       2.times {  page = page.links_with(:text => 'Your Account').first.click }
 
       (page/"tbody#renewcharge tr").each do |book|
-        book_link = book/"a"
+        book_link = book.at("a")
         checkbox = book.at("input") # TODO - on next nokogiri release, change at to %
-        unless book_link.nil? or book_link.empty?
+        unless book_link.nil?
           info = {}
           # RENEW^39100049582508^658.84 SWE^1^Sweeney, Susan, 1956-^101 Internet businesses you can start from home : how to choose and build your own successful e-business^
-          command, info[:id_], callno, status, info[:author], info[:title] = *checkbox['name'].to_s.split(/\^/) if checkbox
-
+          command, catno, callno, status, info[:author], info[:title] = *checkbox['name'].to_s.split(/\^/) if checkbox
           due_date = (book/"td:nth-child(4)").text.strip
-          
-          info[:summary] = "Due on #{due_date}"
-          info[:guid]    = "#{CATALOGUE_URL}#catno#{info[:id_]}" # guid must be unique for each entry
-          
-          f.entry do |entry|
-            info.each{ |tag, value| entry.send(tag, value) }
+
+          f.entry do
+            title    info[:title]
+            author   info[:author]
+            id_      "tag:torontopubliclibrary.ca,2008;#{catno}"
+            updated  Time.now.iso8601
+            link     :rel => 'alternate', :href => "#{CATALOGUE_URL}#{book_link['href']}catno#{catno}"
+            summary do
+              cdata "Due on <strong>#{due_date}</strong> <small>Cat no. #{catno}</small>"
+            end
           end # ENTRY
 
         end
